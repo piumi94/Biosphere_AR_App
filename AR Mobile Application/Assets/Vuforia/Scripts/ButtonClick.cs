1ee66c208fd401ebd4f4767c898557c92282d07b
IT17098588_Piumi <it17098588@my.sliit.lk>
2020-11-05 20:51:08 +0530

    Project added

---
+++ AR Mobile Application/Assets/Vuforia/Scripts/ButtonClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    int index;
+
+    //Use for Initialization
+    public void Start()
+    {
+        index = SceneManager.GetActiveScene().buildIndex;
+    }
+	
+	//Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+	//Navigating to the relevant scene
+    public void changeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+}
